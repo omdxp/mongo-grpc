caa0bacaf3867428b357d743efae4ced64e04513
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-17 09:01:35 +0100

    setup mongodb driver

--- server/server.go
+++ server/server.go
@@ -1,23 +1,49 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/Omar-Belghaouti/mongo-grpc/pb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
+
+var collection *mongo.Collection
 
 type server struct {
 	pb.UnimplementedBlogServiceServer
 }
 
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
+
 func main() {
 	// if we crash the go code, we get the file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// create mongodb client
+	log.Print("connecting to mongodb")
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	collection = client.Database("mydb").Collection("blog")
 
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
@@ -48,6 +74,11 @@ func main() {
 
 	log.Print("closing listener")
 	lis.Close()
+
+	log.Print("closing mongodb connection")
+	if err = client.Disconnect(ctx); err != nil {
+		panic(err)
+	}
 
 	log.Print("bye bye")
 
