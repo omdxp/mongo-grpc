09e15b23cf5c7f6b872d3e9206f9410cfdc87610
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-17 08:42:31 +0100

    setup server with closing signal

--- server/server.go
+++ server/server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"os/signal"
 
 	"github.com/Omar-Belghaouti/mongo-grpc/pb"
 	"google.golang.org/grpc"
@@ -14,6 +16,9 @@ type server struct {
 }
 
 func main() {
+	// if we crash the go code, we get the file name and line number
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatal(err.Error())
@@ -24,7 +29,26 @@ func main() {
 
 	reflection.Register(s)
 
-	if err = s.Serve(lis); err != nil {
-		log.Fatal(err.Error())
-	}
+	go func() {
+		log.Print("starting server")
+		if err = s.Serve(lis); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
+	// wait for control c to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// block until a signal is received
+	<-ch
+
+	log.Print("stopping server")
+	s.Stop()
+
+	log.Print("closing listener")
+	lis.Close()
+
+	log.Print("bye bye")
+
 }
